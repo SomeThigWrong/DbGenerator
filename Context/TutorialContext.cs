1c2cc3a09767ba32b8b4d4a5ecf7f60b1c1b1f71
DESKTOP-G4PM2VM\admin <cuonglb.996@gmail.com>
2024-03-08 09:49:47 +0700

    Add existing project files to Git

---
+++ Context/TutorialContext.cs
@@ -0,0 +1,14 @@
+﻿using DBGenerator.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace DBGenerator.Context
+{
+    public class TutorialDbContext : DbContext
+    {
+        public TutorialDbContext(DbContextOptions<TutorialDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Tutorial> Tutorials { get; set; }
+    }
+}
---
+++ Migrations/20240307081205_SecondMigration.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DBGenerator.Migrations
+{
+    /// <inheritdoc />
+    public partial class SecondMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Content",
+                table: "Tutorials",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Content",
+                table: "Tutorials");
+        }
+    }
+}
